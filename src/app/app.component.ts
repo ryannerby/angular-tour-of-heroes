05b387c781b8ca355bd99ad1125e9a0f4e254d58
Ryan Nerby <ryannerby@Ryans-MacBook-Air.local>
2025-07-02 11:35:16 -0400

    list of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-}
-export interface Hero {
-  id: number;
-  name: string;
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { Hero } from '../heroes';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
+  imports: [CommonModule, FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
+  
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
